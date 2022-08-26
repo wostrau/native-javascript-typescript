3188908085b9fd59ef0f028bcaf808b49b054c57
wostrau <a.ostrovtsov@gmail.com>
2022-08-26 14:26:08 +0300

    DONE - 06 lesson from Native JS playlist / topic: callback

--- src/06/06.tsx
+++ src/06/06.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {ChangeEvent, MouseEvent} from 'react';
 
 export const User = () => {
-    const deleteUser = () => {
-        alert('User was deleted')
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.name)
     };
 
     const saveUser = () => {
         alert('User was saved')
     };
 
+    const onFocuslostHandler = () => {
+        alert('Focus lost!')
+    };
+
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('age changed: ' + event.currentTarget.value)
+    };
+
     return <div>
-        ALex
-        <button onClick={deleteUser}>save</button>
-        <button onClick={saveUser}>delete</button>
+        <textarea onBlur={onFocuslostHandler}>ALex</textarea>
+        <input onChange={onAgeChanged} type={'number'}/>
+        <button name={'deleteUser'} onClick={deleteUser}>save</button>
+        <button name={'saveUser'} onClick={saveUser}>delete</button>
     </div>
 };
--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
         <div className="App">
             {liElements}
             {liUsers}
-
             <User/>
         </div>
     );
